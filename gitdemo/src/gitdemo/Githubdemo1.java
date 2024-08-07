46e6e656f5ade17de5ce2d86235851115a70779b
ramvaddadi <143794792+ramvaddadi@users.noreply.github.com>
2024-08-07 23:55:43 +0530

    07-0824

--- gitdemo/src/gitdemo/Githubdemo1.java
+++ gitdemo/src/gitdemo/Githubdemo1.java
@@ -5,13 +5,10 @@ public class Githubdemo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello");
-
 System.out.println("thisis ram");
-<<<<<<< Updated upstream
+System.out.println("thisis ram from titansteam");
 
-=======
-vnuivlutn
->>>>>>> Stashed changes
+
 	}
 
 }
